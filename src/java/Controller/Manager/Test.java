cff0aa541f3521a284519be8b2b062262eb36156
dong <dongnuc2k3@gmail.com>
2024-05-17 07:51:45 +0700

    chia package

---
+++ src/java/Controller/Manager/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller.Manager;
+
+import Controller.Admin.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class Test {
+    
+}
---
+++ src/java/Model/Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import Controller.Student.*;
+import Controller.Admin.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class Test {
+    
+}
